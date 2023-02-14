fc7c4a6eec2d31fcb9b2c0624c9413de89c0f8bb
Hale <crofth@byu.edu>
2023-02-13 23:34:19 -0700

    Add project files.

---
+++ Mission06_crofth/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_crofth.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_crofth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm (Movie movie)
+        {
+            return View("MovieSubmitted", movie);
+        }
+
+        public IActionResult Podcasts ()
+        {
+            return View("MyPodcasts");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
